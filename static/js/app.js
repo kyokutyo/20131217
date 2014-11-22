c6cf7bb4914c84886c06c0496259130fd36f377f
kyokutyo <haruta@kyokutyo.com>
2014-11-23 01:44:39 +0900

    適当に作品の一覧を取得する URL を組み立てる

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    var api_key = '17570926bf4df161849251ae5cdfaa1b';
+    var user_id = '112437165@N04';
+    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + api_key + '&user_id=' + user_id + '&format=json&nojsoncallback=1';
+    console.log(url);
+
+}());
