ae0b8159a4f5de40076b4819f0de5b4aab2fce14
kyokutyo <haruta@kyokutyo.com>
2014-11-25 23:14:24 +0900

    写真の総数を取得する

--- static/js/app.js
+++ static/js/app.js
@@ -3,15 +3,48 @@
 (function() {
     'use strict';
 
-    var api_key = '17570926bf4df161849251ae5cdfaa1b';
-    var user_id = '112437165@N04';
     var date_before_one_year = '2013-11-23';
-    var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + api_key + '&user_id=' + user_id + '&format=json&nojsoncallback=1';
+    var api_url = 'https://api.flickr.com/services/rest/';
+    var params = {
+        method: 'flickr.photos.search',
+        api_key: '17570926bf4df161849251ae5cdfaa1b',
+        user_id: '112437165@N04',
+        format: 'json',
+        nojsoncallback: '1'
+    };
 
     var App = React.createClass({
+        getInitialState: function() {
+            return {
+                total: undefined
+            };
+        },
+        setPhotoState: function(data) {
+            this.setState({
+                total: parseInt(data.photos.total, 10)
+            });
+        },
+        componentDidMount: function() {
+            var that = this;
+
+            $.ajax(api_url, {
+                dataType: 'json',
+                data: params,
+                success: that.setPhotoState,
+                error: function() {
+                }
+            });
+        },
         render: function() {
+            var link;
+
+            if(typeof this.state.total !== 'undefined') {
+                link = (
+                    <a href={api_url}>{this.state.total}</a>
+                );
+            }
             return (
-                <p>{url}</p>
+                <p>{link}</p>
             );
         }
     });
